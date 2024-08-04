fe27321aa0b7345f84394c594cdf9fc3885998d4
Harsh Singh <harshrajeevsingh@gmail.com>
2024-08-05 00:57:34 +0530

    add: responsiveness to watch Page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,24 +17,25 @@ export const VideoWithBackground = (props) => {
   //   }, [videoRef, canvasRef]);
 
   return (
-    <section className="relative max-w-7xl w-full ">
+    <section className="relative max-w-7xl w-full h-max">
       {" "}
       <video
         ref={videoRef}
         controls
-        className={`md:static fixed top-16 left-0 right-0 lg:w-[65%] md:w-[calc(100%_-_12rem)] lg:mt-7 lg:ml-8 aspect-video md:rounded-xl`} //${isFullscreen ? "p-0" : "p-20"}
+        // className={`md:static fixed top-16 left-0 right-0 lg:w-[60%] xl:w-[65%] lg:mt-7 lg:ml-8 aspect-video md:rounded-xl`}
+        // className={`md:static fixed top-16 left-0 right-0 lg:w-[65%]  lg:mt-7 lg:ml-8 aspect-video md:rounded-xl`}
         {...props}
       />
       <canvas
         width="10"
         height="6"
         aria-hidden="true"
-        className="absolute -top-16 left-0 -z-10 md:w-10/12 w-full h-[700px] dark:opacity-10 opacity-30"
+        className="absolute -top-16 left-0 -z-10 lg:w-10/12 w-full h-[700px] dark:opacity-10 opacity-30"
         ref={canvasRef}
       />
-      <div className="pointer-events-none absolute -top-16 left-0 -z-10 md:w-10/12 w-full h-[700px]">
-        <div className="absolute hidden md:block right-0 top-0 w-32 h-full bg-gradient-to-l from-background to-transparent dark:from-background" />
-        <div className="absolute hidden md:block left-0 top-0 w-14 h-full bg-gradient-to-r from-background to-transparent dark:from-background" />
+      <div className="pointer-events-none absolute -top-16 left-0 -z-10 lg:w-10/12 w-full h-[700px]">
+        <div className="absolute hidden lg:block right-0 top-0 w-32 h-full bg-gradient-to-l from-background to-transparent dark:from-background" />
+        <div className="absolute hidden lg:block left-0 top-0 w-14 h-full bg-gradient-to-r from-background to-transparent dark:from-background" />
         <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-background to-transparent dark:from-background" />
         <div className="absolute left-0 top-0 w-full h-32 bg-gradient-to-b to-10% from-background to-transparent dark:from-background" />
       </div>
--- src/pages/WatchPage.jsx
+++ src/pages/WatchPage.jsx
@@ -2,20 +2,31 @@ import { VideoWithBackground } from "../components/VideoCard";
 
 export const WatchPage = () => {
   return (
-    // <div className="w-96 h-full">
-    <div className="w-full min-h-svh">
-      <VideoWithBackground src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
-      <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    // <div className="w-full min-h-svh">
+    // <div className="w-full">
+    //   <VideoWithBackground src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+    //   <div className="md:mt-0">
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //     <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+    //   </div>
+    // </div>
+    <div className="w-full min-h-svh xl:mt-4 xl:ml-2">
+      <div className="sticky lg:static top-16">
+        <VideoWithBackground
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          className="w-full xl:w-[65%] aspect-video  lg:rounded-xl"
+        />
+      </div>
+      <div>
+        <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+        <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+        <div className="bg-transparent border-5 border-orange-400 w-full h-96"></div>
+      </div>
     </div>
   );
 };
