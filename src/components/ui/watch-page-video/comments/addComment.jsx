58205f44d9de37aab44538db5579c433984a373e
Harsh Singh <harshrajeevsingh@gmail.com>
2024-09-25 20:31:26 +0530

    fix: now only authenticated user can make comment, used loginModal

--- src/components/ui/watch-page-video/comments/addComment.jsx
+++ src/components/ui/watch-page-video/comments/addComment.jsx
@@ -1,127 +1,14 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Avatar, Input, Button } from "@nextui-org/react";
-import { useForm, Controller, useWatch } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 import { useUserStoreSelectors } from "../../../../store/userSlice";
 import { useAddComment } from "../../../../api/commentApi";
+import { LoginModal } from "../../loginModal";
 
-/*
 const AddComment = ({ videoId }) => {
   const user = useUserStoreSelectors.use.user();
-  console.log(videoId);
-  const { mutate: submitComment, isPending, error } = useAddComment(videoId);
-  const { control, handleSubmit, watch, reset } = useForm({
-    defaultValues: {
-      content: "",
-    },
-  });
-
-  const contentText = watch("content");
-
-  const onSubmit = (data) => {
-    console.log(data);
-    if (user) {
-      submitComment(data);
-    } else {
-      console.log("Login to add comment");
-    }
-    reset();
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full flex gap-3 mb-5">
-      <div>
-        <Avatar showFallback src={user?.avatar?.url} />
-      </div>
-      <div className="flex-grow flex items-center">
-        <Controller
-          name="content"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Input
-              {...field}
-              isClearable
-              type="text"
-              variant="underlined"
-              placeholder="Add a comment..."
-              className="flex-grow"
-              onClear={() => reset()}
-            />
-          )}
-        />
-        {contentText && (
-          <Button
-            type="submit"
-            color="primary"
-            size="sm"
-            className="ml-2"
-            isDisabled={isPending}
-          >
-            Post
-          </Button>
-        )}
-      </div>
-      {error && <p>{error.message}</p>}
-    </form>
-  );
-};
-*/
-/*
-const AddComment = ({ videoId }) => {
-  const user = useUserStoreSelectors.use.user();
-  console.log(videoId);
-  const { mutate: submitComment, isPending, error } = useAddComment(videoId);
-  const { handleSubmit, reset, register } = useForm({
-    defaultValues: {
-      content: "",
-    },
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-    if (user) {
-      submitComment(data);
-    } else {
-      console.log("Login to add comment");
-    }
-    reset();
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full flex gap-3 mb-5">
-      <div>
-        <Avatar showFallback src={user?.avatar?.url} />
-      </div>
-      <div className="flex-grow flex items-center">
-        <Input
-          {...register("content", { required: true })}
-          isClearable
-          type="text"
-          variant="underlined"
-          placeholder="Add a comment..."
-          className="flex-grow"
-          onClear={() => reset()}
-        />
-
-        <Button
-          type="submit"
-          color="primary"
-          size="sm"
-          className="ml-2"
-          isDisabled={isPending}
-        >
-          Post
-        </Button>
-      </div>
-      {error && <p>{error.message}</p>}
-    </form>
-  );
-};
-*/
-const AddComment = ({ videoId }) => {
-  const user = useUserStoreSelectors.use.user();
-  console.log(videoId);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const { mutate: submitComment, isPending, error } = useAddComment(videoId);
   const { handleSubmit, reset, register } = useForm({
     defaultValues: {
@@ -139,46 +26,55 @@ const AddComment = ({ videoId }) => {
     if (user) {
       submitComment(data);
     } else {
-      console.log("Login to add comment");
+      setIsLoginModalOpen(true);
     }
     reset();
     setContentText("");
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full flex gap-3 mb-5">
-      <div>
-        <Avatar showFallback src={user?.avatar?.url} />
-      </div>
-      <div className="flex-grow flex items-center">
-        <Input
-          {...register("content", { required: true })}
-          isClearable
-          type="text"
-          variant="underlined"
-          placeholder="Add a comment..."
-          className="flex-grow"
-          onClear={() => {
-            reset();
-            setContentText("");
-          }}
-          onChange={handleContentChange}
-        />
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full flex gap-3 mb-5"
+      >
+        <div>
+          <Avatar showFallback src={user?.avatar?.url} />
+        </div>
+        <div className="flex-grow flex items-center">
+          <Input
+            {...register("content", { required: true })}
+            isClearable
+            type="text"
+            variant="underlined"
+            placeholder="Add a comment..."
+            className="flex-grow"
+            onClear={() => {
+              reset();
+              setContentText("");
+            }}
+            onChange={handleContentChange}
+          />
 
-        {contentText && (
-          <Button
-            type="submit"
-            color="primary"
-            size="sm"
-            className="ml-2"
-            isDisabled={isPending}
-            radius="full"
-          >
-            Comment
-          </Button>
-        )}
-      </div>
-    </form>
+          {contentText && (
+            <Button
+              type="submit"
+              color="primary"
+              size="sm"
+              className="ml-2"
+              isDisabled={isPending}
+              radius="full"
+            >
+              Comment
+            </Button>
+          )}
+        </div>
+      </form>
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        onOpenChange={setIsLoginModalOpen}
+      />
+    </>
   );
 };
 export default AddComment;
