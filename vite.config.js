07a770cd4405ae427c4a260ed210af2795187d5b
harshrajeevsingh <harshrajeevsingh@gmail.com>
2025-01-18 10:17:59 +0530

    tryna deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000/api/v1',
+//         secure: false, // Indicate handling a self-signed certificate
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000/api/v1',
-        secure: false, // Indicate handling a self-signed certificate
-      },
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   },
+  server: {
+    port: 5173,
+  }
 });
